83f479aff40e5f6a6991f48f752a0218849bea12
astucioso <150879600+astucioso@users.noreply.github.com>
2025-12-29 02:24:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const userId1 = '1220165675155456133';
-const userId2 = '1429586882764734574';
+const userId2 = '1420426762172498073';
 
 const avatarElement1 = document.getElementById('avatar1');
 const nicknameElement1 = document.getElementById('nickname-text1');
@@ -53,5 +53,6 @@ async function fetchLanyardData(userId, avatarElement, nicknameElement, subnickE
 fetchLanyardData(userId1, avatarElement1, nicknameElement1, subnickElement1, nitroBadge1, boostBadge1);
 fetchLanyardData(userId2, avatarElement2, nicknameElement2, subnickElement2, nitroBadge2, boostBadge2);
 fetchLanyardData(userId3, avatarElement3, nicknameElement3, subnickElement3, nitroBadge3, boostBadge3); 
+
 
 
