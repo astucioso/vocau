3ffbed350ae72efed1950884bda4fe60bacd8b2e
astucioso <150879600+astucioso@users.noreply.github.com>
2025-12-29 02:50:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
 const userId1 = '1220165675155456133';
-const userId2 = '1420426762172498073';
 
 const avatarElement1 = document.getElementById('avatar1');
 const nicknameElement1 = document.getElementById('nickname-text1');
 const subnickElement1 = document.getElementById('subnick1');
 const nitroBadge1 = document.getElementById('nitro-badge1');
 const boostBadge1 = document.getElementById('boost-badge1');
-
-const avatarElement2 = document.getElementById('avatar2');
-const nicknameElement2 = document.getElementById('nickname-text2');
-const subnickElement2 = document.getElementById('subnick2');
-const nitroBadge2 = document.getElementById('nitro-badge2');
-const boostBadge2 = document.getElementById('boost-badge2');
-
-const avatarElement3 = document.getElementById('avatar3'); 
-const nicknameElement3 = document.getElementById('nickname-text3'); 
-const subnickElement3 = document.getElementById('subnick3');
-const nitroBadge3 = document.getElementById('nitro-badge3'); 
-const boostBadge3 = document.getElementById('boost-badge3');
 
 async function fetchLanyardData(userId, avatarElement, nicknameElement, subnickElement, nitroBadge, boostBadge) {
   try {
@@ -41,7 +28,7 @@ async function fetchLanyardData(userId, avatarElement, nicknameElement, subnickE
         boostBadge.classList.remove('hidden');
       }
     } else {
-      nicknameElement.textContent = 'UsuÃ¡rio nÃ£o encontrado';
+      nicknameElement.textContent = 'Usuário não encontrado';
       subnickElement.textContent = '';
     }
   } catch (error) {
@@ -51,8 +38,3 @@ async function fetchLanyardData(userId, avatarElement, nicknameElement, subnickE
 }
 
 fetchLanyardData(userId1, avatarElement1, nicknameElement1, subnickElement1, nitroBadge1, boostBadge1);
-fetchLanyardData(userId2, avatarElement2, nicknameElement2, subnickElement2, nitroBadge2, boostBadge2);
-fetchLanyardData(userId3, avatarElement3, nicknameElement3, subnickElement3, nitroBadge3, boostBadge3); 
-
-
-
